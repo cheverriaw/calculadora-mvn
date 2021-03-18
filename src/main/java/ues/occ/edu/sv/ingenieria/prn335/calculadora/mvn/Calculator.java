cd514b9285f517ddbb8f49f9d3b270d579958cfe
cheverriaw <willytpi2021@gmail.com>
2021-03-18 09:46:53 -0600

    modifique las dependencias y propiedades

--- src/main/java/ues/occ/edu/sv/ingenieria/prn335/calculadora/mvn/Calculator.java
+++ src/main/java/ues/occ/edu/sv/ingenieria/prn335/calculadora/mvn/Calculator.java
@@ -10,5 +10,19 @@ package ues.occ.edu.sv.ingenieria.prn335.calculadora.mvn;
  * @author william
  */
 public class Calculator {
+     public double add(double number1, double number2){
+        return number1 + number2;
+    }
     
+    public double subtract(double number1, double number2){
+        return number1 - number2;
+    }
+    
+    public double multiply(double number1, double number2){
+        return number1 * number2;
+    }
+    
+    public double divide(double number1, double number2){
+        return number1 / number2;
+    }
 }
